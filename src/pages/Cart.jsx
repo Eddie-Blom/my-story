4428981c837d36ab274a24fde9fa60b1dfa8efdc
Eddie <eddie.sutcu@iths.se>
2023-04-11 08:56:25 +0200

    fixed some issues with cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,34 +1,78 @@
-import { Button, Box, Grid } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function Home() {
+function Cart() {
+  const [cartItems, setCartItems] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(items);
+    fetch("/Products.json")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleRemove = (item) => {
+    const updatedCartItems = cartItems.filter(
+      (cartItem) => cartItem.id !== item.id
+    );
+    setCartItems(updatedCartItems);
+    localStorage.setItem("cart", JSON.stringify(updatedCartItems));
+  };
+
+  const getImageUrl = (itemId) => {
+    const item = products.find((p) => p.id === itemId);
+    return item ? item.Image : "";
+  };
+
   return (
-    <Grid margin={2}>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          minHeight: "50vh",
-        }}
-      >
-        <h2>About Page</h2>
-        <NavLink to="/" style={{ textDecoration: "none" }}>
-          <Button
-            color="success"
-            variant="contained"
-            sx={{ fontWeight: "bold" }}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        margin: "0 -16px",
+      }}
+    >
+      {cartItems.length > 0 ? (
+        cartItems.map((item) => (
+          <Card
+            key={item.id}
+            style={{
+              width: 300,
+              margin: "8px",
+            }}
           >
-            Go to homepage
-          </Button>
-        </NavLink>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          vitae maxime, eum fuga culpa nam, delectus similique porro tenetur
-          quod dicta nesciunt eos iusto? Sequi velit minima vel fugit beatae?
-        </p>
-      </Box>
-    </Grid>
+            <CardMedia
+              component="img"
+              height="200px"
+              image={getImageUrl(item.id)}
+              alt={item.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="h2">
+                {item.name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Pris: {item.price} Kr
+              </Typography>
+              <IconButton onClick={() => handleRemove(item)}>
+                <DeleteIcon />
+              </IconButton>
+            </CardContent>
+          </Card>
+        ))
+      ) : (
+        <h1>Varukorgen är tom!</h1>
+      )}
+    </div>
   );
 }
+
+export default Cart;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Home() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('/Products.json')
+    fetch("/Products.json")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
 
+  const handleAddToCart = (product) => {
+    const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+    cartItems.push(product);
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  };
+
   return (
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-        margin: '0 -16px',
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        margin: "0 -16px",
       }}
     >
       {products.map((product) => (
@@ -27,7 +35,7 @@ function Home() {
           key={product.id}
           style={{
             width: 300,
-            margin: '8px',
+            margin: "8px",
           }}
         >
           <CardMedia
@@ -43,6 +51,9 @@ function Home() {
             <Typography variant="body2" color="textSecondary" component="p">
               Pris: {product.price} Kr
             </Typography>
+            <IconButton onClick={() => handleAddToCart(product)}>
+              <ShoppingCartIcon />
+            </IconButton>
           </CardContent>
         </Card>
       ))}
